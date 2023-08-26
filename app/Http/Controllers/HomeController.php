a44179838c46f97b8b098f55a8708a446d911b19
progRizvi <prog.rizvi@gmail.com>
2023-08-27 01:25:30 +0600

    admin dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Booking;
+use App\Models\CustomerUser;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
-    public function dashboard ()
+    public function dashboard()
     {
-         return view('backend.pages.dashboard');
+        $todayBookings = Booking::whereDate('created_at', Carbon::today())->count();
+        $todayTotalBookings = Booking::whereDate('created_at', Carbon::today())->sum('total_amount');
+        $lastMonthBooking = Booking::whereMonth('created_at', Carbon::now()->subMonth())->count();
+        $customers = CustomerUser::count();
+        // dd($todayBookings, $todayTotalBookings, $lastMonthBooking);
+        return view('backend.pages.dashboard', compact('todayBookings', 'todayTotalBookings', 'lastMonthBooking', "customers"));
     }
-    
+
 }
